70d68e070f73c73853b14753819f502afd75e715
febriansyahnr <febriansyah.rayiid@paper.id>
2025-12-03 12:48:19 +0700

    remove tabs in customer lists

--- app/Filament/Resources/Customers/Pages/ListCustomers.php
+++ app/Filament/Resources/Customers/Pages/ListCustomers.php
@@ -7,23 +7,10 @@ use App\Filament\Resources\Customers\CustomerResource;
 use Filament\Actions\CreateAction;
 use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
-use Filament\Schemas\Components\Tabs\Tab;
-use Filament\Support\Icons\Heroicon;
-use Illuminate\Database\Eloquent\Builder;
 
 class ListCustomers extends ListRecords
 {
     protected static string $resource = CustomerResource::class;
-
-    public function getTabs(): array
-    {
-        return [
-            'all' => Tab::make(),
-            'trash' => Tab::make()
-                ->modifyQueryUsing(fn(Builder $query) => $query->withoutGlobalScopes()->where('deleted_at', '!=', null))
-                ->icon(Heroicon::Trash),
-        ];
-    }
 
     protected function getHeaderActions(): array
     {
