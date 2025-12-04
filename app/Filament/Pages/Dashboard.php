d1f2a476c18c90b5323751ea889cd75d60afeeda
febriansyahnr <febriansyah.rayiid@paper.id>
2025-12-04 10:44:39 +0700

    remove unused code

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -6,31 +6,11 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Pages\Dashboard as BaseDashboard;
 use Filament\Pages\Dashboard\Actions\FilterAction;
 use Filament\Pages\Dashboard\Concerns\HasFiltersAction;
-use Filament\Pages\Dashboard\Concerns\HasFiltersForm;
-use Filament\Schemas\Components\Group;
-use Filament\Schemas\Components\Section;
-use Filament\Schemas\Schema;
+
 class Dashboard extends BaseDashboard
 {
-    // use HasFiltersForm;
     use HasFiltersAction;
 
-    // public function filtersForm(Schema $schema): Schema
-    // {
-    //     return $schema
-    //         ->components([
-    //             Group::make()
-    //                 ->columnSpan(3)
-    //                 ->schema([]),
-    //             Section::make("Filters")
-    //                 ->schema([
-    //                     DatePicker::make('startDate'),
-    //                     DatePicker::make('endDate'),
-    //                 ])
-    //                 ->columns(1)
-    //                 ->columnSpan(1),
-    //         ]);
-    // }
 
     protected function getHeaderActions(): array
     {
@@ -47,5 +27,4 @@ class Dashboard extends BaseDashboard
     {
         return 2;
     }
-
 }
