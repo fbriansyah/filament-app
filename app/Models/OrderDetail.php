cc8f1f7f0a7587892b899262528ec44a5f6a0ec8
febriansyahnr <febriansyah.rayiid@paper.id>
2025-12-05 16:57:52 +0700

    Create Order and OrderDetail models

---
+++ app/Models/OrderDetail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class OrderDetail extends Model
+{
+    use HasUuids, SoftDeletes;
+
+    protected $fillable = [
+        'order_id',
+        'service_id',
+        'assign_to',
+        'price',
+        'status',
+        'scheduled_at',
+        'note',
+    ];
+
+    protected $casts = [
+        'id' => 'uuid',
+        'price' => 'decimal:2',
+        'scheduled_at' => 'datetime',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function service()
+    {
+        return $this->belongsTo(Service::class);
+    }
+
+    public function assignTo()
+    {
+        return $this->belongsTo(User::class, 'assign_to');
+    }
+}
