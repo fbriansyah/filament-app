99ba528862bf3dcb8736a16ad445ec1207466011
febriansyahnr <febriansyah.rayiid@paper.id>
2025-12-12 21:37:35 +0700

    Restructure Order Infolist for Improved Readability

--- app/Filament/Resources/Orders/Schemas/OrderInfolist.php
+++ app/Filament/Resources/Orders/Schemas/OrderInfolist.php
@@ -4,7 +4,10 @@ namespace App\Filament\Resources\Orders\Schemas;
 
 use App\Models\Order;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\TextSize;
 
 class OrderInfolist
 {
@@ -12,30 +15,40 @@ class OrderInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('id')
-                    ->label('ID'),
-                TextEntry::make('code'),
-                TextEntry::make('discount')
-                    ->numeric(),
-                TextEntry::make('scheduled_at')
-                    ->dateTime(),
-                TextEntry::make('status')
-                    ->badge(),
-                TextEntry::make('address')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('note')
-                    ->placeholder('-')
-                    ->columnSpanFull(),
-                TextEntry::make('created_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('updated_at')
-                    ->dateTime()
-                    ->placeholder('-'),
-                TextEntry::make('deleted_at')
-                    ->dateTime()
-                    ->visible(fn (Order $record): bool => $record->trashed()),
+                Section::make('Details')
+                    ->inlineLabel()
+                    ->components([
+                        TextEntry::make('code')
+                            ->weight(FontWeight::Bold)
+                            ->label('Order Code'),
+                        TextEntry::make('order_details_sum_price')
+                            ->sum('orderDetails', 'price')
+                            ->label('Price')
+                            ->money("IDR", locale: "id"),
+                        TextEntry::make('discount')
+                            ->money("IDR", locale: "id"),
+                        TextEntry::make('scheduled_at')
+                            ->dateTime(),
+                        TextEntry::make('status')
+                            ->badge()
+                    ])
+                ,
+                Section::make('Customer Infos')
+                    ->inlineLabel()
+                    ->components([
+                        TextEntry::make('customer.name')
+                            ->label('Name'),
+                        TextEntry::make('customer.phone')
+                            ->label('Phone'),
+                        TextEntry::make('customer.email')
+                            ->label('Email'),
+                        TextEntry::make('address')
+                            ->placeholder('-')
+                            ->columnSpanFull(),
+                        TextEntry::make('note')
+                            ->placeholder('-')
+                            ->columnSpanFull(),
+                    ]),
             ]);
     }
 }
